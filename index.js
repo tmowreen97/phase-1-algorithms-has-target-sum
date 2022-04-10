67e18bb7817fcf72a28f4016354183c35525bc9a
Tasreen Mowreen <tmowreen97@gmail.com>
2022-04-10 19:22:36 -0400

    commit tree f4e74a04f5880fa2f587103713385f2fef77a8bc

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 function hasTargetSum(array , target) {
   for (let i =0 ; i< array.length ; i++){
-      console.log(`we're starting with i position ${i} and value ${array[i]}`)
       if (array.length<2){
           return false
       } else if (array[i] > target){
-          return false
+          i++
       }
       else if (array[i] === target){
-          for (let j = i+1 ; j <array.length ; j++){
-              if (array[j] === 0) {
-                  console.log(`we're looking for a value of 0`)
-                  return true
-              } else { 
-                  console.log('no 0!')
-                  return false
-              }
+          for (const number of array){
+            isItZero(number)
           }
       }
-      else if (array[i]<target) {
-          for (let k =i+1 ; k< array.length; k++){
+      else {
+          for (let j =i+1 ; j< array.length; j++){
               let diff = target - array[i]
-              console.log(`the diff is equal to ${diff} so we are looking for that array value. currently k is ${k} and value is ${array[k]}`)
-              if (array[k] === diff){
+              if (array[j] === diff){
                   return true
-              } else if (array[k]!==diff) {
-                  k++
-                  console.log(`the diff should be the same ${diff}, but k is ${k} array value is ${array[k]}`)
-                  if (k === array.length){
+              } else {
+                  j++
+                  if (j === array.length){
                       return false
                   }
-                  else if (array[k] === diff){
+                  else if (array[j] === diff){
                       return true
                   }
-              }
-              
+              } 
           }
       }
   }
 }
 
+function isItZero(number){
+  if (number===0) {
+    return true
+  } else {
+    return false
+  }
+}
 /* 
   Write the Big O time complexity of your function here
   NEED TO CLEAN UP!!!
@@ -46,26 +43,37 @@ function hasTargetSum(array , target) {
 
 /* 
   Add your pseudocode here
-  function takes two arguments, array and target.
-  first we have to iterate through the array
-  We are adding pairs together and obtaining the value.
-    We can iterate through array, grab the first index, subtract that from the target value and see if the difference is a number also in the array. 
-  compare each value to the target. 
-  at the end we get either true or false.
-  return true if we have a pair that adds up to the target. 
-    WE ONLY NEED ONE PAIR!
-  return false if we do not.
+
+  With constant i, for each element i in array,
+  if the length of the array is less than 2, return false.
+  else if the i element of the array is greater than the target, 
+    add 1 to i and move on to the next element.
+  else if the i element is equal to the target, 
+    define a function to find if anyother element in the array is equal to 0. if not, add 1 to i and move onto the next element.
+  else loop another constant j right ahead of the i element. 
+    with j we can check the remaining elements in the array while keeping the array[i] value intact. 
+    calculate the difference between the target and array[i]. 
+    we need to find this difference value among the remaining elements in the array using j. 
+    if the j element does not equal the difference, 
+      add 1 to j. 
+    else if j is equal to the length of the array,
+      return false because we haven't found it. 
+    else if array[j] is equal to the difference,
+      return true because we found it. 
+
+
+
 */
 
 /*
   Add written explanation of your solution here
 
-  First, make a for loop starting with grabbing the first value of the array.
-    If that selection totals to GREATER THAN OR EQUAL TO the target, move on to the next value.
-    Else (if) subtract that value from the target value. 
-      If the difference is a value within the array return TRUE
-      Else move onto the next value
-  ELSE Return false.
+  For this problem we need to keep in mind, we only need ONE PAIR. Once we find this pair, we return true.
+  Calculate the current difference with the current element and the target. 
+  Use another constant to iterate through the array looking for the difference. 
+  if we don't find the current difference, move onto the next element and find the new difference.
+  continue like this until we reach the last element in the array. 
+  Remember to start by showing what will falsify the function, if there is less than 2 elements in the array, if the element is equal to the target and there isn't a '0' in the array (target + 0 = target).
 
 
 */
@@ -85,6 +93,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting:true ")
+  console.log("=>", hasTargetSum([30,2,6,7,1], 13));
 }
 
 module.exports = hasTargetSum;
